92c7d990b669f1f059b2ddc0a00338876577c7b7
archermarc085 <archermarc085@gmail.com>
2016-04-11 00:31:54 +0300

    update

--- EF-Repository/WinFormsNoDevExp/Properties/AssemblyInfo.cs
+++ EF-Repository/WinFormsNoDevExp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EF-Repository")]
+[assembly: AssemblyTitle("WinFormsNoDevExp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EF-Repository")]
+[assembly: AssemblyProduct("WinFormsNoDevExp")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c70cef3-16dc-441a-bbf3-8c9e9499c947")]
+[assembly: Guid("7a408cc8-384e-4b20-bedb-92097c845474")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("WinFormsEF")]
-[assembly: InternalsVisibleTo(" WinFormsNoDevExp")]
